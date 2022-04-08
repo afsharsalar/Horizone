329005b6e79ce0ad30ec459fc970f6b91a5e9012
salar afshar <afshar.salar@live.com>
2022-04-08 21:12:21 +0430

    Add project files.

---
+++ MyTask/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MyTask
+{
+    interface IHttpGetExtention
+    {
+        void Execute(string url,ref string output);
+    }
+
+    class StringUseCase : IHttpGetExtention
+    {
+        public void Execute(string url,ref string output)
+        {
+            using (var client = new HttpClient())
+            {
+                var httpContent =Task.Run(() => client.GetAsync(url)) ;
+                
+                var data =  Task.Run(() => httpContent.Result.Content.ReadAsStringAsync());
+                output = data.Result;
+                //deserilize for different data
+            }
+
+        }
+
+       
+    }
+
+    class IntUseCase : IHttpGetExtention
+    {
+        public void Execute(string url, ref string output)
+        {
+            using (var client = new HttpClient())
+            {
+                var httpContent = Task.Run(() => client.GetAsync(url));
+
+                var data = Task.Run(() => httpContent.Result.Content.ReadAsStringAsync());
+                output = data.Result;
+                //deserilize for different data
+            }
+
+        }
+    }
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            var stringUserCase = new StringUseCase();
+
+            string resultForStringUSeCase="";
+            stringUserCase.Execute("https://kaverin-ddb.firebaseio.com/interview/useCase/string.json", ref resultForStringUSeCase);
+
+
+            var intUseCase = new IntUseCase();
+
+            string resultForIntUseCas="";
+            intUseCase.Execute("https://kaverin-ddb.firebaseio.com/interview/useCase/int.json",ref resultForIntUseCas);
+
+
+            Console.WriteLine("Result for string use case :"+ resultForStringUSeCase);
+            Console.WriteLine("Result for int use case :"+ resultForIntUseCas);
+            
+        }
+    }
+}
