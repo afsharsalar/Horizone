6547053c7555b782dce9af7a325f0125bb9c2861
salar afshar <afshar.salar@live.com>
2022-04-08 21:52:16 +0430

    Emitter UseCase

--- MyTask/Program.cs
+++ MyTask/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace MyTask
 {
@@ -42,12 +43,83 @@ namespace MyTask
 
         }
     }
+
+
+
+
+
+   
+    class EmitterUseCase
+    {
+        public event EventHandler Start;
+        public event EventHandler Error;
+        public event EventHandler Next;
+        public event EventHandler Stop;
+
+
+        
+
+        public void Trigger()
+        {
+            try
+            {
+
+
+
+                OnStart(EventArgs.Empty);
+                var intUseCase = new IntUseCase();
+                var result = "";
+                intUseCase.Execute("https://kaverin-ddb.firebaseio.com/interview/useCase/int.json",ref result);
+                Console.WriteLine("Result is:"+result);
+
+                OnNext(EventArgs.Empty);
+
+            }
+            catch (Exception e)
+            {
+                OnError(EventArgs.Empty);
+            }
+
+        }
+
+        public void OnStart(EventArgs e)
+        {
+            Console.WriteLine("Start at:"+DateTime.Now.ToString("HH:mm:ss tt"));
+            Start?.Invoke(this,e);
+        }
+
+        public void OnNext(EventArgs e)
+        {
+            Console.WriteLine("data fetched successfully at:" + DateTime.Now.ToString("HH:mm:ss"));
+            Next?.Invoke(this, e);
+        }
+
+        public void OnError(EventArgs e)
+        {
+            Console.WriteLine("Error....");
+            Error?.Invoke(this, e);
+        }
+
+        public void OnStop(EventArgs e)
+        {
+            Console.WriteLine("Stopped at:" + DateTime.Now.ToString("HH:mm:ss tt"));
+            Stop?.Invoke(this, e);
+        }
+
+        public void Cancel()
+        {
+            Console.WriteLine("Cancel callback");
+            // Perform object cancellation here.
+        }
+    }
+
+
     internal class Program
     {
 
         static void Main(string[] args)
         {
-
+            //StringUseCase and IntUseCase
             var stringUserCase = new StringUseCase();
 
             string resultForStringUSeCase="";
@@ -62,7 +134,31 @@ namespace MyTask
 
             Console.WriteLine("Result for string use case :"+ resultForStringUSeCase);
             Console.WriteLine("Result for int use case :"+ resultForIntUseCas);
+
+
+
+            //Emitter UseCase
+
             
+            var timer = new Timer(TimeSpan.FromSeconds(5).TotalMilliseconds); // Set the time (5 second in this case)
+            timer.AutoReset = true;
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(CallEmitter);
+            timer.Start();
+
+            Console.WriteLine("Emitter UseCase start in 5 second, if you want to cancel it enter 0");
+            var input=Console.ReadLine();
+            if (input == "0")
+            {
+                timer.Stop();
+            }
         }
+
+        private static void CallEmitter(object sender, ElapsedEventArgs e)
+        {
+            var instance = new EmitterUseCase();
+            instance.Trigger();
+        }
+
+
     }
 }
